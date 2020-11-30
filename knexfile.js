106c8da4fe0e1074a81a5d0e4edb390f615ca0a1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-11-29 21:08:54 -0300

    Teste

--- knexfile.js
+++ knexfile.js
@@ -30,13 +30,17 @@ module.exports = {
   },
 
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    searchPath: 'knex,public',
-    pool: { min: 0, max: 7 },
-    migrations: [
-      "./src/database/migrations/"
-    ],
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
+
+};
 
 };
