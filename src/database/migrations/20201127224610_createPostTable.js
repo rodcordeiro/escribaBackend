ea42b8090648de0fa33920d6b737bcfe5d4e64ce
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-11-29 22:41:15 -0300

    Update migrations

--- src/database/migrations/20201127224610_createPostTable.js
+++ src/database/migrations/20201127224610_createPostTable.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('chapters',function(table){
         table.increments();
         table.string('title').notNullable();
-        table.string('text',999999999).notNullable();
+        table.text('text').notNullable();
     })
   
 };
